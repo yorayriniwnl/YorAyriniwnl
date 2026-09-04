9d4891198d44f068ffb2cb22e2959bb33fe34d8b
YorAyriniwnl <yorayriniwnl@gmail.com>
2026-09-05 00:10:40 +0530

    feat: rebuild vision art

--- scripts/generate_readme.py
+++ scripts/generate_readme.py
@@ -101,7 +101,7 @@ ASSET_REVISIONS = {
     "project-portfolio-mobile-v2.svg": "raster-v8",
     "project-helios.svg": "raster-v13",
     "project-zenith.svg": "raster-v14",
-    "project-vision.svg": "raster-v14",
+    "project-vision.svg": "raster-v15",
     "project-talks.svg": "raster-v13",
     "project-token-usage.svg": "raster-v13",
     **{filename: "atlas-v6" for filename in PROJECT_SUMMARIES.values()},
